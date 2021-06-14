31b048e00683aed6b4854b57a126ccf85d3d20be
arthur-carneiro <arthur.carneiro@ee.ufcg.edu.br>
2021-06-14 01:54:12 -0300

    Atualizacoes palpites

---
+++ mysite/myapp/rodadas.py
@@ -0,0 +1,10 @@
+
+def return_rodadas_dict():
+    Rodadas_dict = {
+        'Rodada 3': 'https://docs.google.com/forms/d/e/1FAIpQLScKy2fbLDx4XIUdjna-gcuxuyJfuJXQYqmtAxnVK9eVqbrBNw/viewform?embedded=true',
+        'Rodada 4': 'https://docs.google.com/forms/d/e/1FAIpQLSeqN86F71prME82L6bWJR_OYs9QKiDC_ZZ0cY6q3SgIOeWxxA/viewform?embedded=true',
+        'Rodada 5': 'https://docs.google.com/forms/d/e/1FAIpQLSfHN2W9hcNk6NSF-NyDDjui92iXZPyiMrSqQEO5MJVHm9-NZw/viewform?embedded=true',
+        'Rodada 6': 'https://docs.google.com/forms/d/e/1FAIpQLSeWUNt2M8_R6bWrhK11n5sco5Y4W6Vh26WLLdYzUxbNhrRQxQ/viewform?embedded=true',
+        'Rodada 7': 'https://docs.google.com/forms/d/e/1FAIpQLSeWGF9zIuPEVN4PWHpjxnSq4YKyJ0l82EWIgOCbXIIXoO25yA/viewform?embedded=true'
+    }
+    return Rodadas_dict
